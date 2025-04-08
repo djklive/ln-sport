7bbe80b65d91a2c50fdfb0e2bc577b6629066b1a
djklive <franckdjk@gmail.com>
2025-04-08 13:17:43 +0100

    correct contact page

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import Link from "next/link";
+
 
 const Header: React.FC = () => {
   return (
@@ -64,7 +64,6 @@ const Header: React.FC = () => {
                 Actualités
               </Link>
             </li>
-            <li>
             <li>
               <Link href="/contact" className="hover:text-orange-500">
                 Contact
